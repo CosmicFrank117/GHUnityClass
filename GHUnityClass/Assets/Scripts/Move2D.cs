c79a1b4d30c0264e0d52a5e257826479520ec549
CosmicFrank117 <frank58928@yahoo.com>
2022-05-14 12:15:26 -0700

    2D Stuff

---
+++ GHUnityClass/Assets/Scripts/Move2D.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move2D : MonoBehaviour
+{
+    Rigidbody2D rb;
+
+    void Start() 
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update() 
+    {
+        
+    }
+}
